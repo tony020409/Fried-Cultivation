ced5dce5cb95bddbd0ef7bb360b44fbd1e8289f1
FireGod <tony020409@gmail.com>
2017-05-31 19:34:25 +0800

    2017/5/31,19:34

---
+++ Assets/Scripts/BagController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagController : MonoBehaviour {
+
+
+    public static bool isPaintBool;//可種植bool
+    public new static string name;//作物名稱
+    public static int PainTime;//此作物種植所需時間
+    void Awake () {
+        isPaintBool = false;
+        
+    }
+
+
+
+    public void Button_Click_Close() {
+        gameObject.SetActive(false);
+        isPaintBool = false;
+    }
+    //漢堡
+    public void Button_Click_Hamburger() {
+        //假如錢夠
+        isPaintBool = true;
+        name = "漢堡";
+        PainTime = 1;
+    }
+    //漢堡(暫時)
+    public void Button_Click_Hamburger2()
+    {
+        //假如錢夠
+        isPaintBool = true;
+        name = "漢堡";
+        PainTime = 1;
+    }
+    public void Button_Click_Hamburger3()
+    {
+        //假如錢夠
+        isPaintBool = true;
+        name = "漢堡";
+        PainTime = 1;
+    }
+}
--- Assets/Scripts/FarmStatus.cs
+++ Assets/Scripts/FarmStatus.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public enum Status {
     //空,種植,收成 狀態
     Null,
@@ -13,21 +13,67 @@ public enum Status {
 public class FarmStatus : MonoBehaviour {
 
     private Status status;//狀態
-
+    private GameObject bag;//包包
+    public UISprite Painter;//作物圖片
+    private int PaintTime;//作物所需時間
+    private DateTime StartPaintTime;//作物種植的時間點;
+    private TimeSpan AfterPaintTime;//作物經過的時間;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        bag = GameObject.Find("Bag");
+        
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (BagController.isPaintBool == true && status == Status.Null)
+        {
+            gameObject.GetComponent<UISprite>().color = Color.green;
+        }
+        else if (BagController.isPaintBool == true && status == Status.Painting)
+        {
+            gameObject.GetComponent<UISprite>().color = Color.red;
+        }
+        else if (BagController.isPaintBool == true && status == Status.Geting)
+        {
+            gameObject.GetComponent<UISprite>().color = Color.red;
+        }
+        else {
+            gameObject.GetComponent<UISprite>().color = Color.white;
+        }
+
+        //
+        if (status == Status.Painting) {
+            if ((DateTime.Now - StartPaintTime).TotalMinutes >= 1) {
+                print("成熟了");
+            }
+        }
+    }
     //第一次點擊田
     public void FarmClick() {
-        if (status == Status.Null) {
+        if (status == Status.Null)//無作物 狀態
+        {
+            if (BagController.isPaintBool == false)//不是種植bool
+            {
+                bag.SetActive(true);
+            }
+            else if(BagController.isPaintBool == true)//是種植bool
+            {
+                Painter.spriteName = BagController.name;
+                StartPaintTime = DateTime.Now;
+                status = Status.Painting;
+            }
+        }
+        else if (status == Status.Painting)//種植中 狀態
+        {
+
+        }
+        else if (status == Status.Geting)//可收成 狀態
+        {
 
         }
     }
+    
 }
